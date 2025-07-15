9cd0f40c3163862ea447a7e03716c60a4b7f7f0c
Fajar Mustaqin <fajar.mustaqin01@gmail.com>
2025-07-15 14:55:16 +0700

    create table product dan api endpoint

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_produk' => 'required|string',
+            'sku' => 'required|string|unique:products',
+            'harga' => 'required|numeric',
+            'stok' => 'required|integer',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        Product::destroy($id);
+        return response()->json(['message' => 'Produk dihapus']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
