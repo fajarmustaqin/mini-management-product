57bac0ca11128be4d0773ff75b506530f2014063
Fajar Mustaqin <fajar.mustaqin01@gmail.com>
2025-07-15 22:42:42 +0700

    tambahan model add data error

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'nama_produk',
+        'sku',
+        'harga',
+        'stok'
+    ];
 }
